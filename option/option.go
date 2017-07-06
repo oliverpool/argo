d6e07a91de7ff3c6f54646c06aa5bdee97f1b549
oliverpool <oliverpool@hotmail.fr>
2017-07-06 12:26:12 +0200

    improve doc

--- option/option.go
+++ option/option.go
@@ -1,5 +1,7 @@
 // Package option gathers some of the options the aria2 methods (or the daemon) may take in argument.
 // Full list on https://aria2.github.io/manual/en/html/aria2c.html#id2
+//
+// For a custom option, simply do argo.Option{"key": "value"}
 package option
 
 import (
