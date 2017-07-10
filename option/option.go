aac67623f32b52dbccc71af4667a7811408e80f9
oliverpool <oliverpool@hotmail.fr>
2017-07-10 15:19:19 +0200

    add Out and Dst options

--- option/option.go
+++ option/option.go
@@ -5,10 +5,26 @@
 package option
 
 import (
+	"path/filepath"
+
 	"github.com/oliverpool/argo"
 )
 
 // Dir sets the directory to store the downloaded file.
 func Dir(dir string) argo.Option {
 	return argo.Option{"dir": dir}
+}
+
+// Out sets the file name of the downloaded file.
+// It is always relative to the directory given in --dir option. When the --force-sequential option is used, this option is ignored.
+//
+// Note: You cannot specify a file name for Metalink or BitTorrent downloads. The file name specified here is only used when the URIs fed to aria2 are given on the command line directly, but not when using --input-file, --force-sequential option.
+func Out(file string) argo.Option {
+	return argo.Option{"out": file}
+}
+
+// Dst sets the destination file (combination of Dir and Out).
+func Dst(fullpath string) argo.Option {
+	dir, file := filepath.Split(fullpath)
+	return argo.Option{"dir": dir, "out": file}
 }
